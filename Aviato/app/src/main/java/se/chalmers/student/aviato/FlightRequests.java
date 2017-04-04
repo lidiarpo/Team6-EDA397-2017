bc6100e1b0886838be58bb43b6a81adbbca63dd9
Berto Gomez <csberto@gmail.com>
2017-04-04 16:04:50 +0200

    Added skeleton for flight requests to the API

--- Aviato/app/src/main/java/se/chalmers/student/aviato/FlightRequests.java
+++ Aviato/app/src/main/java/se/chalmers/student/aviato/FlightRequests.java
@@ -1,4 +1,49 @@
 package se.chalmers.student.aviato;
 
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
+import static com.android.volley.VolleyLog.TAG;
+
 public class FlightRequests {
-}
+
+    private RequestQueueSingleton queue;
+
+    public void getArrivals(String airportCode, int year, int month, int day, int hour, int timeWindow, Context context,
+                                   Response.Listener<JSONObject>listener, Response.ErrorListener errorListener){
+            queue = RequestQueueSingleton.getInstance(context);
+
+        String url = "https://api.flightstats.com/flex/flightstatus/rest/v2/json/airport/status/GOT/dep/"
+                + Integer.toString(year)+  "/" + Integer.toString(month)+ "/" + Integer.toString(day)
+                + "/" + Integer.toString(day) + "/" + Integer.toString(hour) + "?appId="+ Utilities.APPID +
+                "&appKey=" + Utilities.APPKEY + "&utc=false&numHours=" + Integer.toString(timeWindow);
+
+            JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                    (Request.Method.GET, url, null, listener, errorListener);
+
+            queue.addToRequestQueue(jsObjRequest);
+        }
+
+    public void getDepartures(String airportCode, int year, int month, int day, int hour, int timeWindow, Context context,
+                                     Response.Listener<JSONObject>listener, Response.ErrorListener errorListener){
+
+        queue = RequestQueueSingleton.getInstance(context);
+        String url = "https://api.flightstats.com/flex/flightstatus/rest/v2/json/airport/status/GOT/arr/"
+                + Integer.toString(year)+  "/" + Integer.toString(month)+ "/" + Integer.toString(day)
+                + "/" + Integer.toString(day) + "/" + Integer.toString(hour) + "?appId="+ Utilities.APPID +
+                "&appKey=" + Utilities.APPKEY + "&utc=false&numHours=" + Integer.toString(timeWindow);
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, listener, errorListener);
+
+        queue.addToRequestQueue(jsObjRequest);
+    }
+    }
