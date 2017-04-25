394a05efca498c0ca8557cbf7417ec6e2597b1a4
Berto Gomez <csberto@gmail.com>
2017-04-25 22:04:04 +0200

    Service now updates subscriptions based on API information #8 #10

--- Aviato/app/src/main/java/se/chalmers/student/aviato/Utilities.java
+++ Aviato/app/src/main/java/se/chalmers/student/aviato/Utilities.java
@@ -1,6 +1,9 @@
 package se.chalmers.student.aviato;
 
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class Utilities {
 
     public final static String APPID = "0eae5c29";
@@ -8,4 +11,10 @@ public class Utilities {
 
     public final static String API_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
     public final static String VIEW_DATE_FORMAT = "HH:mm";
+
+    public static Calendar toCalendar(Date date){
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        return cal;
+    }
 }
