d18d3dd01fe6859f1421643942d28dda857b302b
Lídia Nyman <lidia.rpo@gmail.com>
2017-04-25 13:32:20 +0200

    Initial Flight Details Overview

--- Aviato/app/src/main/java/se/chalmers/student/aviato/FlightActivity.java
+++ Aviato/app/src/main/java/se/chalmers/student/aviato/FlightActivity.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.content.Intent;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -13,6 +15,7 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Set;
 
 
 public class FlightActivity extends Activity{
@@ -23,16 +26,35 @@ public class FlightActivity extends Activity{
     private Response.Listener<JSONObject> listener;
     private Response.ErrorListener errorListener;
     private FlightRequests flightRequests;
+    private FlightRequests flightDetails;
 
     ListView flightlistView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flight);
 
         // The listview to populate
         flightlistView = (ListView) findViewById(R.id.lvFlightContainer);
+
+        flightlistView.setClickable(true);
+
+        flightlistView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Flight flight = (Flight)flightlistView.getItemAtPosition(position);
+                //setContentView(R.layout.flight_overview);
+                //OverviewAdapter adapter = new OverviewAdapter(, arrayoftheFlightData);
+
+                Intent loadOverviewActivity = new Intent(FlightActivity.this, OverviewActivity.class);
+
+                loadOverviewActivity.putExtra("flight", flight.toString());
+                startActivity(loadOverviewActivity);
+
+            }
+        });
 
 
         //TODO: Make Flight attributes look differently in the listview now the Flight object is
@@ -57,12 +79,12 @@ public class FlightActivity extends Activity{
         };
     }
 
-    //TODO: make OnItemClick method for the flightlistView
-
     @Override
     protected void onStart() {
         super.onStart();
         flightRequests  = new FlightRequests(this);
+
+        flightDetails = new FlightRequests(this);
         //TO-DO Modify in order to select Departures
 
         Calendar rightNow = Calendar.getInstance();
