ec1747f86d8c3d7ccb9dac06cbc597519c55cd0e
Berto Gomez <csberto@gmail.com>
2017-04-25 15:11:26 +0200

    Added Flight Subscriptions Database, CRUD methods

---
+++ Aviato/app/src/main/java/se/chalmers/student/aviato/DB/FlightsContract.java
@@ -0,0 +1,25 @@
+package se.chalmers.student.aviato.DB;
+
+import android.provider.BaseColumns;
+
+public class FlightsContract {
+        private FlightsContract() {}
+
+        public static class FlightEntry implements BaseColumns {
+            public static final String TABLE_NAME = "flights";
+            public static final String COLUMN_NAME_FLIGHT_ID = "flightId";
+            public static final String COLUMN_NAME_CARRIER_FS_CODE = "carrierFsCode";
+            public static final String COLUMN_NAME_FLIGHT_NUMBER = "flightNumber";
+            public static final String COLUMN_NAME_DEPARTURE_AIRPORT_FS_CODE = "departureAirportFsCode";
+            public static final String COLUMN_NAME_ARRIVAL_AIRPORT_FS_CODE = "arrivalAirportFsCode";
+            public static final String COLUMN_NAME_DEPARTURE_DATE = "departureDate";
+            public static final String COLUMN_NAME_ARRIVAL_DATE = "arrivalDate";
+            public static final String COLUMN_NAME_STATUS = "status";
+            public static final String COLUMN_NAME_FLIGHT_TYPE = "flightType";
+            public static final String COLUMN_NAME_FLIGHT_DURATIONS = "flightDurations";
+            public static final String COLUMN_NAME_DEPARTURE_TERMINAL = "departureTerminal";
+            public static final String COLUMN_NAME_DEPARTURE_GATE = "departureGate";
+            public static final String COLUMN_NAME_ARRIVAL_TERMINAL = "arrivalTerminal";
+            public static final String COLUMN_NAME_ARRIVAL_GATE = "arrivalGate";
+        }
+    }
