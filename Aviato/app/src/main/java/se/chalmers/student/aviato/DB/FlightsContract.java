a4903dbada09c14803bdad5eddffe6593c012358
Berto Gomez <csberto@gmail.com>
2017-04-28 16:22:43 +0200

    Refactored database scheme (new fields), fixed wrong usages of dates on the app.

--- Aviato/app/src/main/java/se/chalmers/student/aviato/DB/FlightsContract.java
+++ Aviato/app/src/main/java/se/chalmers/student/aviato/DB/FlightsContract.java
@@ -21,5 +21,10 @@ public class FlightsContract {
             public static final String COLUMN_NAME_DEPARTURE_GATE = "departureGate";
             public static final String COLUMN_NAME_ARRIVAL_TERMINAL = "arrivalTerminal";
             public static final String COLUMN_NAME_ARRIVAL_GATE = "arrivalGate";
+            public static final String COLUMN_NAME_CARRIER_NAME = "carrierName";
+            public static final String COLUMN_NAME_DEPARTURE_AIRPORT_NAME = "departureAirportName";
+            public static final String COLUMN_NAME_ARRIVAL_AIRPORT_NAME = "arrivalAirportName";
+            public static final String COLUMN_NAME_SCHEDULED_GATE_DEPARTURE = "scheduledGateDeparture";
+            public static final String COLUMN_NAME_SCHEDULED_GATE_ARRIVAL = "scheduledGateArrival";
         }
     }
--- Aviato/app/src/main/java/se/chalmers/student/aviato/DB/FlightsDbHelper.java
+++ Aviato/app/src/main/java/se/chalmers/student/aviato/DB/FlightsDbHelper.java
@@ -10,7 +10,7 @@ import static se.chalmers.student.aviato.DB.Constants.SQL_DELETE_ENTRIES;
 
 public class FlightsDbHelper extends SQLiteOpenHelper {
     // If you change the database schema, you must increment the database version.
-    public static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 2;
     public static final String DATABASE_NAME = "Flights.db";
 
     public FlightsDbHelper(Context context) {
