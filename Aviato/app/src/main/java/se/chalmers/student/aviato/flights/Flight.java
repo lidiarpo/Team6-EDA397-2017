763a3ab22310767c3d17066a7c21445f4e1a196a
platisd <kurukii88@gmail.com>
2017-04-30 18:42:54 +0200

    getTime() utility function for Flights
    Return the date of the arrival or departure (depending on the flight) as a Calendar instance

--- Aviato/app/src/main/java/se/chalmers/student/aviato/flights/Flight.java
+++ Aviato/app/src/main/java/se/chalmers/student/aviato/flights/Flight.java
@@ -1,13 +1,26 @@
 package se.chalmers.student.aviato.flights;
 
+import android.util.Log;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+import se.chalmers.student.aviato.Utilities;
+
+/**
+ *  This will be our result object
+ *
+ * Created by gryphex on 2017-04-04.
+ */
 
 public class Flight{
     //Attributes for our FlightObject
-    private Map<String, String> flightAttributes = new HashMap();
+    private Map<String, String> flightAttributes = new HashMap<>();
     public final static String INVALID_ATTRIBUTE = "INVALID ATTRIBUTE";
     public final static String NOT_AVAILABLE = "N/A";
     private final String [] flightAttributeArray = {"flightId","carrierFsCode","carrierName", "flightNumber",
@@ -51,6 +64,37 @@ public class Flight{
      */
     public String set(String attributeKey, String attributeValue) {
         return flightAttributes.containsKey(attributeKey) ? updateValue(attributeKey, attributeValue) : INVALID_ATTRIBUTE;
+    }
+
+    /**
+     * Get the time (date) of the flight as a Calendar instance
+     * @return a calendar with the time of the arrival if this is an arrival or the departure otherwise
+     */
+    public Calendar getTime() {
+        SimpleDateFormat sdf = new SimpleDateFormat(Utilities.API_DATE_FORMAT);
+        try {
+            // The date we are interested in depends on whether this is an arrival or a departure
+            String date = this.isArrival() ? this.get("arrivalDate") : this.get("departureDate");
+            // Try to parse the supplied string that represents a date
+            Date d = sdf.parse(date);
+            // If successful then set the appropriate date
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(d);
+            return calendar;
+        } catch (ParseException ex) {
+            // Should we throw an exception here and crash instead?
+            Log.e("Flight", "Error parsing the supplied date");
+        }
+        // Return null to indicate a failure to parse the date
+        return null;
+    }
+
+    /**
+     * Check to see if this is an arrival or a departure, depending on the arrival airport's code
+     * @return true if the flight is towards Gothenburg
+     */
+    public boolean isArrival(){
+        return this.get("arrivalAirportFsCode").equals("GOT");
     }
 
     /**
