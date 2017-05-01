4f13dc2d67c24134c1e40cd90c06c79a3c51d8f0
platisd <kurukii88@gmail.com>
2017-05-01 02:12:22 +0200

    Method to retrieve a list of dummy flights for testing

--- Aviato/app/src/main/java/se/chalmers/student/aviato/Utilities.java
+++ Aviato/app/src/main/java/se/chalmers/student/aviato/Utilities.java
@@ -1,8 +1,14 @@
 package se.chalmers.student.aviato;
 
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+import se.chalmers.student.aviato.flights.Flight;
 
 public class Utilities {
 
@@ -16,5 +22,78 @@ public class Utilities {
         Calendar cal = Calendar.getInstance();
         cal.setTime(date);
         return cal;
+    }
+
+    /**
+     * Generate a list of dummy flights that can be used for testing purposes
+     * @return a list of dummy flights
+     */
+    public static List<Flight> getDummyFlights() {
+        final long ONE_MINUTE = 1000 * 60;
+        final long ONE_HOUR = ONE_MINUTE * 60;
+
+        List<Flight> dummyFlights = new ArrayList<Flight>();
+        Calendar now = Calendar.getInstance();
+        SimpleDateFormat apiDateFormat = new SimpleDateFormat(Utilities.API_DATE_FORMAT);
+        Random rnd = new Random();
+        rnd.setSeed(now.getTimeInMillis());
+
+
+        Flight f1 = new Flight();
+        f1.set("flightId", String.valueOf(rnd.nextInt(1000)));
+        f1.set("flightNumber", "AO105");
+        f1.set("arrivalAirportFsCode", "GOT");
+        f1.set("departureAirportFsCode", "ARN");
+        Calendar arrival = Calendar.getInstance();
+        arrival.setTimeInMillis(now.getTimeInMillis() + 50 * ONE_MINUTE); // Arriving in 50 minutes from now
+        String arrivalDate = apiDateFormat.format(arrival.getTime());
+        Calendar departure = Calendar.getInstance();
+        departure.setTimeInMillis(now.getTimeInMillis() - ONE_HOUR);
+        String departureDate = apiDateFormat.format(departure.getTime());
+        f1.set("departureDate", departureDate);
+        f1.set("arrivalDate", arrivalDate);
+
+        Flight f2 = new Flight();
+        f2.set("flightId", String.valueOf(rnd.nextInt(1000)));
+        f2.set("flightNumber", "SAS123");
+        f2.set("arrivalAirportFsCode", "ARN");
+        f2.set("departureAirportFsCode", "GOT");
+        arrival.setTimeInMillis(now.getTimeInMillis() + 2 * ONE_HOUR);
+        arrivalDate = apiDateFormat.format(arrival.getTime());
+        departure.setTimeInMillis(now.getTimeInMillis() + 55 * ONE_MINUTE); // Departing in 55 minutes from now
+        departureDate = apiDateFormat.format(departure.getTime());
+        f2.set("departureDate", departureDate);
+        f2.set("arrivalDate", arrivalDate);
+
+        Flight f3 = new Flight();
+        f3.set("flightId", String.valueOf(rnd.nextInt(1000)));
+        f3.set("flightNumber", "BA129299");
+        f3.set("arrivalAirportFsCode", "BEJ");
+        f3.set("departureAirportFsCode", "GOT");
+        arrival.setTimeInMillis(now.getTimeInMillis() + 11 * ONE_HOUR);
+        arrivalDate = apiDateFormat.format(arrival.getTime());
+        departure.setTimeInMillis(now.getTimeInMillis() + 65 * ONE_MINUTE); // Departing in 65 minutes from now
+        departureDate = apiDateFormat.format(departure.getTime());
+        f3.set("departureDate", departureDate);
+        f3.set("arrivalDate", arrivalDate);
+
+        Flight f4 = new Flight();
+        f4.set("flightId", String.valueOf(rnd.nextInt(1000)));
+        f4.set("flightNumber", "AEGEAN29299");
+        f4.set("arrivalAirportFsCode", "GOT");
+        f4.set("departureAirportFsCode", "ATH");
+        arrival.setTimeInMillis(now.getTimeInMillis() + 70 * ONE_MINUTE); //Arriving in 1 hour and 10 minutes from now
+        arrivalDate = apiDateFormat.format(arrival.getTime());
+        departure.setTimeInMillis(now.getTimeInMillis() - 4 * ONE_HOUR);
+        departureDate = apiDateFormat.format(departure.getTime());
+        f4.set("departureDate", departureDate);
+        f4.set("arrivalDate", arrivalDate);
+
+        dummyFlights.add(f1);
+        dummyFlights.add(f2);
+        dummyFlights.add(f3);
+        dummyFlights.add(f4);
+
+        return dummyFlights;
     }
 }
