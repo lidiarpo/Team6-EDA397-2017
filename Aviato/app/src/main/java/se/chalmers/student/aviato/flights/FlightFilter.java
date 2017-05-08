e86e244abf1e412e7931f6ae8af46ddf2d0b9e95
Jobaer <java2.cloudcentrik@gmail.com>
2017-05-08 17:04:24 +0300

    TestingFlightFilter

---
+++ Aviato/app/src/main/java/se/chalmers/student/aviato/flights/FlightFilter.java
@@ -0,0 +1,8 @@
+package se.chalmers.student.aviato.flights;
+
+/**
+ * Created by jobaer-pc on 04-May-17.
+ */
+
+public interface FlightFilter {
+}
---
+++ Aviato/app/src/test/java/se/chalmers/student/aviato/FilterTest.java
@@ -0,0 +1,8 @@
+package se.chalmers.student.aviato;
+
+/**
+ * Created by jobaer-pc on 04-May-17.
+ */
+
+public class FilterTest {
+}
