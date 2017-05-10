fe0a207ebbddb8c3e2ebcb0cf3921915e8bc7f25
platisd <kurukii88@gmail.com>
2017-05-10 22:41:04 +0200

    Share application with friend via email
    * Through the settings activity the user can tip a friend (send an email)
    about Aviato.
    * Added link to project's repository
    
    Related to #117

--- Aviato/app/src/main/java/se/chalmers/student/aviato/settings/SettingsFragment.java
+++ Aviato/app/src/main/java/se/chalmers/student/aviato/settings/SettingsFragment.java
@@ -1,8 +1,10 @@
 package se.chalmers.student.aviato.settings;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
-
 
 import se.chalmers.student.aviato.R;
 
@@ -13,5 +15,32 @@ public class SettingsFragment extends PreferenceFragment {
         super.onCreate(savedInstanceState);
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
+
+        Preference shareButton = findPreference(getString(R.string.share_button_title));
+        shareButton.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                final String mailSubject = getString(R.string.email_subject);
+                final String mailBody = getString(R.string.email_body);
+
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                Uri data = Uri.parse("mailto:?subject=" + mailSubject + "&body=" + mailBody);
+                intent.setData(data);
+                startActivity(intent);
+                return true;
+            }
+        });
+
+        Preference credits = findPreference(getString(R.string.credits_title));
+        credits.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                Uri data = Uri.parse("https://github.com/bertogs/Team6-EDA397-2017/");
+                intent.setData(data);
+                startActivity(intent);
+                return true;
+            }
+        });
     }
 }
