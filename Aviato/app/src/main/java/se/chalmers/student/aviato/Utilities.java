9077c914ad76e86195b979946ccd336085fd8dcf
Jobaer <java2.cloudcentrik@gmail.com>
2017-05-11 10:21:40 +0300

    FlightStatsAccountInfoUpdated

--- Aviato/app/src/main/java/se/chalmers/student/aviato/Utilities.java
+++ Aviato/app/src/main/java/se/chalmers/student/aviato/Utilities.java
@@ -18,8 +18,8 @@ import se.chalmers.student.aviato.flights.Flight;
 
 public class Utilities {
 
-    public final static String APPID = "0a1973b0";
-    public final static String APPKEY = "18c1fc7152a12bcf3f8be5ebb3fd34f2";
+    public final static String APPID = "e61930fd";
+    public final static String APPKEY = "6600ef91f513710790e874fa6e1bd257";
 
     public final static String API_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
     public final static String VIEW_DATE_FORMAT = "HH:mm";
